89f17ef34b9f9607969ab1b221fcb087ca685b93
fbakly <fouadelbakly130@gmail.com>
2020-06-09 10:41:16 +0200

    Removed individual notification functions and made it more modular

--- batteryNotifier.go
+++ batteryNotifier.go
@@ -11,8 +11,12 @@ import (
 	"github.com/mqu/go-notify"
 )
 
-func playBeep() {
-	f, err := os.Open("/home/fbakly/.scripts/resources/beep.mp3")
+var beepPath string = "/home/fbakly/.scripts/resources/beep.mp3"
+var inboxPath string = "/home/fbakly/.scripts/resources/filling-your-inbox.mp3"
+var notificationTime time.Duration = 2
+
+func playAudio(pathToAudio string) {
+	f, err := os.Open(pathToAudio)
 	if err == nil {
 		streamer, format, err := mp3.Decode(f)
 		if err == nil {
@@ -30,66 +34,38 @@ func playBeep() {
 	}
 }
 
-func playInbox() {
-	f, err := os.Open("/home/fbakly/.scripts/resources/filling-your-inbox.mp3")
-	if err == nil {
-		streamer, format, err := mp3.Decode(f)
-		if err == nil {
-			defer streamer.Close()
-			speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-			done := make(chan bool)
-			speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-				done <- true
-			})))
-			<-done
-			close(done)
-			f.Close()
-		}
-	}
+func sendNotification(title string, content string, icon string, urgency notify.NotifyUrgency, finished chan bool) {
+	notification := notify.NotificationNew(title, content, icon)
+	notify.NotificationSetUrgency(notification, urgency)
+	notification.Show()
+	time.Sleep(notificationTime * time.Second)
+	notify.NotificationClose(notification)
+	finished <- true
 }
 
-func notifyFullBattery(notified *bool) {
+func notifyBatteryLevel(title string, content string, icon string, urgency notify.NotifyUrgency, notified *bool) {
 	if !*(notified) {
+		finished := make(chan bool)
 		*notified = true
-		fullBattery := notify.NotificationNew("Battery Full", "", "dialog-information")
-		notify.NotificationSetUrgency(fullBattery, notify.NOTIFY_URGENCY_LOW)
-		fullBattery.Show()
-		playBeep()
-		time.Sleep(2 * time.Second)
-		notify.NotificationClose(fullBattery)
-	}
-}
-
-func notifyLowBattery(notified *bool) {
-	if !(*notified) {
-		*notified = true
-		batteryLow := notify.NotificationNew("Battery Low", "Charge Now!!!", "dialog-information")
-		notify.NotificationSetUrgency(batteryLow, notify.NOTIFY_URGENCY_CRITICAL)
-		batteryLow.Show()
-		playBeep()
-		time.Sleep(2 * time.Second)
-		notify.NotificationClose(batteryLow)
+		go sendNotification(title, content, icon, urgency, finished)
+		playAudio(beepPath)
+		<- finished
 	}
 }
 
 func notifyCharge(isCharging bool, chargeNotified *bool) {
 	if isCharging && !(*chargeNotified) {
+		finished := make(chan bool)
 		*chargeNotified = true
-		charging := notify.NotificationNew("Charging battery", "", "dialog-information")
-		notify.NotificationSetUrgency(charging, notify.NOTIFY_URGENCY_NORMAL)
-		charging.Show()
-		playInbox()
-		time.Sleep(2 * time.Second)
-		notify.NotificationClose(charging)
+		go sendNotification("Charging Battery", "", "dialog-information", notify.NOTIFY_URGENCY_NORMAL, finished)
+		playAudio(inboxPath)
+		<- finished
 	} else if !isCharging && *chargeNotified {
+		finished := make(chan bool)
 		*chargeNotified = false
-		discharging := notify.NotificationNew("Battery Discharging", "", "dialog-information")
-		notify.NotificationSetUrgency(discharging, notify.NOTIFY_URGENCY_NORMAL)
-		discharging.Show()
-		playInbox()
-		time.Sleep(2 * time.Second)
-		notify.NotificationClose(discharging)
+		go sendNotification("Battery Discharging", "", "dialog-information", notify.NOTIFY_URGENCY_NORMAL, finished)
+		playAudio(inboxPath)
+		<- finished
 	}
 }
 
@@ -126,9 +102,9 @@ func main() {
 		percentage /= len(batteries)
 
 		if percentage == 100 {
-			notifyFullBattery(&notified)
+			notifyBatteryLevel("Battery Full", "", "dialog-information", notify.NOTIFY_URGENCY_NORMAL, &notified)
 		} else if percentage <= 20 {
-			notifyLowBattery(&notified)
+			notifyBatteryLevel("Battery Low", "Charge Now !!!", "dialog-information", notify.NOTIFY_URGENCY_CRITICAL, &notified)
 		} else {
 			notified = false
 		}
